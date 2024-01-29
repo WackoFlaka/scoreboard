8cfa9e1bc04d05b4b83474568702e29cba44144b
Joaquin Chavez <jchav5601@gmail.com>
2024-01-29 16:44:53 -0700

    updated challenge from Jake

--- app.js
+++ app.js
@@ -6,90 +6,48 @@ let away = 0
 let homeTotal = 0
 let awayTotal = 0
 
-// NOTE ADDING POINTS 
 
-function addOneHome() {
-    home += 1
-    homeTotal = document.getElementById('home')
-    homeTotal.innerHTML = home
-    winGame()
+
+
+// NOTE Just one function cause Jake gave me a challenge :(( 
+function game(team, num) {
     
-}
-
-function addThreeHome() {
-    home += 3
-    homeTotal = document.getElementById('home')
-    homeTotal.innerHTML = home
-    winGame()
-}
-
-function addOneAway() {
-    away += 1
-    awayTotal = document.getElementById('away')
-    awayTotal.innerHTML = away
-    
-    winGame()
-}
-
-function addThreeAway() {
-    away += 3 
-    awayTotal = document.getElementById('away')
-    awayTotal.innerHTML = away
-    winGame()
-}
-
-// NOTE Subtracting Points
-
-function lessHome() {
-    home -= 1 
-    homeTotal = document.getElementById('home')
-    homeTotal.innerHTML = home
-    
-    if(home < 0 ) {
-        home = 0
-        homeTotal = document.getElementById('home')
-        homeTotal.innerHTML = home
+    if(team == 'home') {
+        home += num
+        document.getElementById('home').innerHTML = home
+        homeTotal = home
+    } else {
+        away += num
+        document.getElementById('away').innerHTML = away
+        awayTotal = away
     }
-}
-
-function lessAway() {
-    away -= 1
-    awayTotal = document.getElementById('away')
-    awayTotal.innerHTML = away
-    
-    if(away < 0) {
-        away = 0
-        awayTotal = document.getElementById('home')
-        awayTotal.innerHTML = away
-    }
-}
-
-
-// NOTE winners
-
-function winGame() {
-    
-    
-    
-    if(home >= 20) {
-        console.log("Home Team wins!")
-        document.getElementById("homeCard").style.display = "block"
-        document.getElementById("background").style.pointerEvents = "none";
-        document.getElementById("background").style.backgroundColor = "rgba(0, 0, 0, 0.8)"
-        document.getElementById("background").style.opacity = "0.4"
-    } else if(away >= 20) {
+   
+    // NOTE prevents the scores to go negative
+   if(home < 0) {
+    home = 0
+    document.getElementById('home').innerHTML = home
+   } else if(away < 0) {
+    away = 0
+    document.getElementById('away').innerHTML = away
+   }
+   
+   // NOTE which team wins
+   if(home >= 20) {
+    console.log("Home Team wins!")
+    document.getElementById("homeCard").style.display = "block"
+    document.getElementById("background").style.pointerEvents = "none";
+    document.getElementById("background").style.backgroundColor = "rgba(0, 0, 0, 0.8)"
+    document.getElementById("background").style.opacity = "0.4"
+   } else if(away >= 20) {
         console.log("Away Team wins!")
         document.getElementById("awayCard").style.display = "block"
         document.getElementById("background").style.pointerEvents = "none";
         document.getElementById("background").style.backgroundColor = "rgba(0, 0, 0, 0.8)"
         document.getElementById("background").style.opacity = "0.4"
-    }
-}
-
-// NOTE Resets Score
-
-function reset() {
-
+   }
+   
+   // NOTE resets the game
+   if(team == null || num == 0) {
     document.getElementById("background").style.backgroundColor = "rgba(0, 0, 0, 0.411)"
     document.getElementById("background").style.pointerEvents = "visible";
     document.getElementById("background").style.opacity = "1"
@@ -101,6 +59,6 @@ function reset() {
     away = 0
     document.getElementById("away").innerHTML = 0
     document.getElementById("awayCard").style.display = "none"
-    
-    console.log(clearInterval)
+   }
+   
 }
